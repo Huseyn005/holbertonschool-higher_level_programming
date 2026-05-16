4d0d0fc7393443dceb8b97c974f3d6e76cce7e9b
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-16 16:16:42 +0000

    task

--- python-import_modules/tmp/4-hidden_discovery.py
+++ python-import_modules/tmp/4-hidden_discovery.py
@@ -2,10 +2,7 @@
 import hidden_4
 
 if __name__ == "__main__":
-    # Get all names defined in the hidden_4 module
     names = dir(hidden_4)
-    
-    # Filter out names that start with "__" and print them
     for name in names:
         if not name.startswith("__"):
             print("{}".format(name))
