e43d32bec939485b4d74074170e640ed92e6953e
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-16 20:25:27 +0000

    task

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    # This syntax updates the value if key exists, or creates it if it doesn't
+    a_dictionary[key] = value
+    return a_dictionary
