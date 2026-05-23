78c3a1e1a08e486ba0005a1065c2a6fb3172bea3
Huseyn005 <huseyn.mammadli.05@gmail.com>
2026-05-23 11:09:33 +0000

    task

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,10 @@
+-- HSYN
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT genre_id
+    FROM tv_show_genres
+    INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
